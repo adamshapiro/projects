bd6179b493529a1e6c8103e41771bf87d546b7e1
adamshapiro531 <adam.shapiro595@gmail.com>
2014-08-09 15:15:35 -0400

    answer to euler problem 2 solved

--- euler-v1.rb
+++ euler-v1.rb
@@ -1,9 +1,22 @@
 def euler1(num)
-	a=[]
+	a=0
 	(1..(num-1)).each do |i|
 		if (i%3==0 || i%5==0)
-			a << i
+			a+=i
 		end
 	end
-	a.inject{|sum,i| sum+i}
+	return a
+end
+
+def euler2(num)
+	a,b,c,d=0,0,1,1
+	while a<=num do
+		a=c+d
+		if a%2==0
+			b+=a
+		end
+		c=d
+		d=a
+	end
+	return b
 end
