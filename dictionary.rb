53e3fa1aaef8a8c2ed073aded1b5921fb9a1cd6e
adamshapiro531 <adam.shapiro595@gmail.com>
2014-09-04 00:04:43 -0400

    today i learned about chomp

--- dictionary.rb
+++ dictionary.rb
@@ -6,12 +6,8 @@ f = File.open('C:\Users\Adam Shapiro\Desktop\dictionary.txt')
 
 def anagram
 	@data.each do |text|
-		if @dict_hash.key?((text.split('').sort - ["\n"]).join) == true
-			@dict_hash[(text.split('').sort - ["\n"]).join] << text
-		else
-			@dict_hash[(text.split('').sort - ["\n"]).join] = []
-			@dict_hash[(text.split('').sort - ["\n"]).join] << text
-		end
+		text.chomp!
+		@dict_hash[text.split('').sort.join] << text
 	end
 	j = @dict_hash.keys.first
 	@dict_hash.each do |k,v|
