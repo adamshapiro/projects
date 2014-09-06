145616e1ae6afca0b0e048638ddbaf9f970902c5
adamshapiro531 <adam.shapiro595@gmail.com>
2014-09-05 23:13:02 -0400

    needs a lot of work

--- dogmaker-missing.rb
+++ dogmaker-missing.rb
@@ -6,6 +6,6 @@ end
 
 class DogFactory
 	def self.method_missing(meth)
-		/^create_(.*)/.match(meth.to_s)[1] = Class.new(Dog)
+		/^create_(.*)/.match(meth) {|m| m[1] = Class.new(Dog)}
 	end
 end
--- dogmaker.rb
+++ dogmaker.rb
@@ -1,11 +1,13 @@
-class Dog; end
+class Dog
+	def self.bark
+		"bark like a #{self.class.name}"
+	end
+end
 
 class DogFactory
 	def self.create dog_name
 		dog_name = Class.new (Dog) do
-			def self.bark
-				"bark like a #{self.class.name}"
-			end
+			
 		end
 	end
 end
